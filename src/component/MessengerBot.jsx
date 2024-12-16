a7488e19ae77a7b7dc97011b5e62ac6ae80e77c9
rudalkunwar <ruzalkunwar@gmail.com>
2024-12-16 12:27:11 +0545

    messenger bot

---
+++ src/component/MessengerBot.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FacebookProvider, CustomChat } from 'react-facebook';
+
+function MessengerBot() {
+    return (
+        <FacebookProvider appId="592047586550815" chatSupport>
+            <CustomChat pageId="542291682293940" minimized={false} />
+        </FacebookProvider>
+    )
+}
+
+export default MessengerBot
