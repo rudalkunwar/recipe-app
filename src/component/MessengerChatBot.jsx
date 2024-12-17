34a4d75ae22812e6453c510f516bb42c646c82e0
rudalkunwar <ruzalkunwar@gmail.com>
2024-12-17 18:47:02 +0545

    css fixes

--- src/component/MessengerChatBot.jsx
+++ src/component/MessengerChatBot.jsx
@@ -3,15 +3,17 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const MessengerChatBot = ({ pageId, appId }) => {
     return (
-        <FacebookProvider appId={appId}>
-            <CustomChat
-                pageId={pageId}
-                minimized="false"
-                themeColor="#0084ff"
-                loggedInGreeting="Hi! How can we help you today?"
-                loggedOutGreeting="Hi! Please log in to chat with us."
-            />
-        </FacebookProvider>
+        <div className='fixed z-50 top-0 right-0'>
+            <FacebookProvider appId={appId}>
+                <CustomChat
+                    pageId={pageId}
+                    minimized="true"
+                    themeColor="#0084ff"
+                    loggedInGreeting="Hi! How can we help you today?"
+                    loggedOutGreeting="Hi! Please log in to chat with us."
+                />
+            </FacebookProvider>
+        </div>
     );
 };
 
