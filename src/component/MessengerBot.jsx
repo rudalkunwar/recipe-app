af34892da68de657dda299e04d1c7a179494fddf
rudalkunwar <ruzalkunwar@gmail.com>
2024-12-17 14:00:04 +0545

    fixes

--- src/component/MessengerBot.jsx
+++ src/component/MessengerBot.jsx
@@ -4,7 +4,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 function MessengerBot() {
     return (
         <FacebookProvider appId="592047586550815" chatSupport>
-            <CustomChat pageId="542291682293940" minimized={false} />
+            <CustomChat pageId="542291682293940" minimized={true} />
         </FacebookProvider>
     );
 }
