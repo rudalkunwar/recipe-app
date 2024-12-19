1816432e5a21a688c910144e1b8a7df9d5639fc5
rudalkunwar <ruzalkunwar@gmail.com>
2024-12-19 12:57:44 +0545

    integrate messenger chat plugin

--- src/component/MessengerChat.jsx
+++ src/component/MessengerChat.jsx
@@ -1,37 +1,33 @@
 import React, { useEffect } from 'react';
 
 const MessengerChat = () => {
-  useEffect(() => {
-    // Load Facebook SDK
-    window.fbAsyncInit = function() {
-      window.FB.init({
-        xfbml: true,
-        version: 'v18.0'
-      });
-    };
+    useEffect(() => {
+        // Load the Facebook SDK script dynamically when the component is mounted
+        const script = document.createElement('script');
+        script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        script.async = true;
+        script.onload = () => {
+            window.FB.init({
+                xfbml: true,
+                version: 'v15.0',
+            });
+        };
+        document.body.appendChild(script);
+    }, []);
 
-    // Load the SDK asynchronously
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }, []);
-
-  return (
-    <div>
-      {/* Messenger Chat Plugin Code */}
-      <div id="fb-root"></div>
-      <div 
-        className="fb-customerchat"
-        attribution="biz_inbox"
-        page_id="517579444770810"
-      ></div>
-    </div>
-  );
+    return (
+        <div>
+            {/* This div will hold the Facebook Messenger chat widget */}
+            <div
+                className="fb-customerchat"
+                attribution="setup_tool"
+                page_id="517579444770810"  // Replace with your actual Facebook page ID
+                theme_color="#0084ff"  // Customize the theme color
+                logged_in_greeting="Hi! How can we help you?"
+                logged_out_greeting="Goodbye!">
+            </div>
+        </div>
+    );
 };
 
 export default MessengerChat;
